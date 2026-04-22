97167bbebf4ab38714a2046390cb6ae1ce4d7535
mymicute <mirabel2009akah@gmail.com>
2026-04-22 18:48:59 +0100

    fix next config duplicate variable

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,12 +1,5 @@
 import Stripe from "stripe"
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-})
-
-export const DEPOSIT_AMOUNT = 1000
-
 export const SUBSCRIPTION_PRICES = {
-  basic: 1000,
-  premium: 5000,
+  basic: { amount: 1000, name: "Basic Plan" },
+  premium: { amount: 5000, name: "Premium Plan" },
 }
